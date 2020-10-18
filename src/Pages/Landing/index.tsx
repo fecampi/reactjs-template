2976affaa042e834588e9327074a7ce381ca3106
fecampi <felipe.campinho@hotmail.com>
2020-10-17 22:16:43 -0300

    first commit

---
+++ src/Pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../../components/Button';
+
+const Landing: React.FC = () => (
+  <>
+    <h1>Landing</h1>
+    <Button />
+  </>
+);
+
+export default Landing;
---
+++ src/assets/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+    --color-background: #F000F0;
+    --font-roboto:16px "Roboto Slab", serif;
+
+
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+
+  body {
+    background: var(--color-background);
+    color: #FFF;
+    -webkit-font-smoothing: antialiased
+  }
+
+  body, input, button {
+    font: var(--font-roboto);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  h1,h2,h3,h4,h5,h6, strong {
+    font-weight:500;
+  }
+`;
